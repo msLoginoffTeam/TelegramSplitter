3ca5710c4891530b098cb3edbb819b1bd6d9dbf6
sitdikov <SitdikovYUM-ext@vip.cbr.ru>
2025-06-22 14:04:20 +0700

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 docker pull brawleryura1/telegram-splitter-app:latest
 docker stop telegram-splitter-app || true
 docker rm telegram-splitter-app || true
-docker-compose up telegram-splitter-app -d
+ docker-compose up -d telegram-splitter-app
