8bac55d2b802fb17ab25023ba4e99b972309d441
msLoginoff <146209985+msLoginoff@users.noreply.github.com>
2025-07-20 15:52:34 +0700

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+docker network inspect my-network >/dev/null 2>&1 || \
+  docker network create my-network
+
 docker-compose stop telegram-splitter-app || true
 docker-compose rm -f telegram-splitter-app || true
 
